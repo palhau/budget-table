6f4c6e1008d9bad9f42d45d9b1d5de8817cf6f0f
Alan Palhau <alanvieirapalhau@hotmail.com>
2023-08-12 21:19:55 -0300

    feat: data table component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import BudgetTable from './components/BudgetTable';
 import Header from './components/Header';
 import './styles/globals.css';
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
+      <BudgetTable />
     </div>
   );
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Header = () => {
   return (
     <section className="container mt-20">
-      <div className="flex py-2 rounded-lg bg-white font-mono">
-        <h1 className="content-center font-bold text-left text-2xl px-4 py-2 bg-inherit">
+      <div className="flex py-2 border rounded-lg border-black font-mono">
+        <h1 className="content-center font-bold text-left text-2xl px-4 py-2">
           Personal Budget App
         </h1>
       </div>
